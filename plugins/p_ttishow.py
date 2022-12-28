334b9c307481e05f9459f30268f3b65171d33ab5
RAFIQ-CLOUDZ <115889457+RAFIQ-CLOUDZ@users.noreply.github.com>
2022-12-29 03:09:54 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -55,10 +55,14 @@ async def save_group(bot, message):
                         pass
                 temp.MELCOW['welcome'] = await message.reply_video(
                 video="https://telegra.ph/file/03691465baa774e46506d.mp4",                                               
-                                                 caption=f'<b>ʜᴇʏ, {u.mention} 👋🏻\nᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ ɢʀᴏᴜᴘ {message.chat.title}\n\n You Can Find Any Movies and Series ! if you need any movie then then enter the movie name + years. 👍\n\nGuys Enter Only movie Or Webseries Name like This 👇\nVikram ❌\nVikram Tamil Bruh ✅\nVikram 2022  ❌\nThe Boys ✅\nThe Boys S01 ✅\nThe Boys all season ❌</b>',
+                                                 caption=f'<b>ʜᴇʏ, {u.mention} 👋🏻\nᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ ɢʀᴏᴜᴘ {message.chat.title}\n\n You Can Find Any Movies and Series ! if you need any movie then then enter the movie name + years 👍\n\nEnter Only Movie Name like Thiz👇\n\n\nVikram ✅\nVikram 2022 ✅\nVikram Tamil Bruh ❌\n\nSeries Like Thiz👇\n\nThe Boys Season 1 ✅\n The Boys ❌\nThe Boys all season ❌</b>',
+
                                                  reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🔥  ↭ Main Channel ↭  🔥', url='http://t.me/MROTTTamilOffl') ],
+
                                                                                       [ InlineKeyboardButton('🔥  ↭ Request Channel ↭  🔥', url='http://t.me/MR_OTT_REQUEST') ]
+
                                                                                     ] )
+
                 )
                 
 @Client.on_message(filters.command('leave') & filters.user(ADMINS))
