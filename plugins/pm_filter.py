68e2a5ee645b177a50fd30b288e742f4a46da948
RAFIQ-CLOUDZ <115889457+RAFIQ-CLOUDZ@users.noreply.github.com>
2022-12-28 20:17:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -432,13 +432,13 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('⚚ Add Me To Your Groups ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
-            InlineKeyboardButton('⚡ SUBSCᏒIBΞ ⚡', url='https://t.me/MROTTTamilOffl'),
-            InlineKeyboardButton('🤖 UᎮDΛTΞS 🤖', url='https://t.me/MROTTTamilOffl')
+            InlineKeyboardButton('⚡ CHANNEL ⚡', url='https://t.me/MROTTTamilOffl'),
+            InlineKeyboardButton('⚡ DISCUSSION ⚡', url=f"https://t.me/MR_OTT_REQUEST')
         ], [
-            InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
-            InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
+            InlineKeyboardButton('♻️ HELP ♻️', callback_data='help'),
+            InlineKeyboardButton('♻️ ABOUT ♻️', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
