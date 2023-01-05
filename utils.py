8f51d666a90b21d7dc1fe4e7926d401e1e96c2b5
RAFIQ-CLOUDZ <115889457+RAFIQ-CLOUDZ@users.noreply.github.com>
2023-01-05 15:38:48 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -381,7 +381,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://cac.teckypress.in/api'
+    url = f'https://link.tnlink.in/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
